935d9108b883c9527023d5ee0cf6402246034696
nitr80 <egeozturkdev@gmail.com>
2026-04-13 16:10:20 +0200

    implement auth

---
+++ src/store/authStore.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand"
+import type { User } from "../types/user";
+
+interface AuthStore {
+  token: string | null;
+  user: User | null;
+
+  setAuth: (token: string, user: User) => void;
+  logout: () => void;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  token: null,
+  user: null,
+
+  setAuth: (token, user) => set({ token, user }),
+
+  logout: () => set({ token: null, user: null }),
+}));
